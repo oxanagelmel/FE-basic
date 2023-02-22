05ee9f404c03a3705f312c7864b922f6cfae36b8
Gelmel Oxana <oxana.gelmel@googlemail.com>
2023-02-23 00:16:37 +0100

    changed lesson10

--- lesson10/scripts/index.js
+++ lesson10/scripts/index.js
@@ -60,6 +60,19 @@ console.log(isEven(5));
  * и возвращает массив из чисел от 0 до указанного числа. 
  */
 
+function generateArray(value) { // в () аргумент
+    const result = []; // нужно создать/объявить массив
+    for(let i=0; i<=value; i++) {
+        result.push(i);   // результат пушим в массив
+    }
+    return result; // return = возвращает массив
+}
+
+console.log(generateArray(10));
+console.log(generateArray(0));
+console.log(generateArray(100));
+
+
 // Функции (function expression) - функциональное выражение
 // функция внутри выражения
 
@@ -85,19 +98,102 @@ console.log(byeBye());
 
 // сначала объевляю функцию а потом вызываю
 
-// const user = {
-//     username: "Oxana",
-//     jobTitle:"Frontend dev",
-//     city: "Ingolstadt",
-//     age: "28",
-//     showData: function() {
-//         let uerData = "";
-   
-//         for(let key in userS) {
-//             if(key !== "showData") {
-//                 userData = + `${key} : ${user[key]}`
-//             }
-//         }
-//         return userData;
-//     }
+// Когда нам могут быть полезны functional expression?
+// --> когда работаем с методами в объектах
+// Метод у объекта это функция которая ассоциируется с этим объектом.
+
+const user = {
+    username: "Oxana",
+    jobTitle: "Frontend dev",
+    city: "Ingolstadt",
+    age: "28",
+    showData: function() {  //showData = метод
+        let userData = "";
+
+        for(let key in user) {
+            if(key !== "showData") { // не просматриваем showData
+                userData = userData + `${key}: ${user[key]} ` // конкатенация ключ : значение
+            }
+        }
+        return userData;
+    }
+}
+
+console.log(user.showData());
+
+// Стрелочные функуии
+
+// let someFunction = (value1, value2,...) => expression
+// создаётся функция, принимает в себя аргументы, вычесляет выражение и возвращает нам результат
+// --> сокращённый вариант функционального выражения
+
+let sum = (value1, value2) => value1 + value2; //упрощённый вариант вля возврвта значения если нету доп. логики
+
+let sumNew = (value1, value2) => { // -"-
+    //some additional logic
+    return value1 + value2
+};
+
+let square = n => n**2;      // если у нас только один аргумент
+
+let shoppingCartTitle = () => 'Title'; // когда вообше нету аргументов нужны ()
+
+const res = sum(2, 3);
+console.log(res);
+
+// => полезны когда нужно сократить код
+
+
+// Переписать функцию power в виде стрелочной (powerArrow)
+// //function power(startValue, powerValue) {
+//     const result = startValue**powerValue;
+//     return result;
 // }
+
+let powerArrow =(startValue, powerValue) => startValue**powerValue;
+
+console.log(powerArrow(8,2));
+
+
+// document.documentElement = html
+// document.body = body
+// document.head = head
+// childNodes, firstChild, lastChild
+
+// Поиск элементов в ДОМе
+// getElementById
+
+let uniquePrg = document.getElementById('unique-prg');
+console.log(uniquePrg);
+uniquePrg.style.backgroundColor = 'red';
+
+// querySelectorAll
+const prgs = document.querySelectorAll('p');
+console.log(prgs);
+
+const blogPost = document.querySelectorAll('.blog-post');
+console.log(blogPost);
+
+const headline = blogPost[0].querySelector('h3'); //tag, .class, #id
+console.log(headline);
+
+// getElementsByTagName = querySelectorAll(tag)
+// getElementsByClassName = querySelectorAll('.class-name')
+// getElementsByName - искал по аттрибуту name
+
+
+// Задача
+// Добавить на страницу header
+// Внутрь положить nav
+// Внутрь nav положить 2 a (ссылки)
+// В JS найти header, nav & эти ссылки
+// Вывести в консоль
+
+const headerLine = document.querySelector('header');
+console.log(headerLine);
+
+const navLine = headerLine.querySelector('nav');
+console.log(navLine);
+
+const aLinks = navLine.querySelectorAll('a');
+console.log(aLinks);
